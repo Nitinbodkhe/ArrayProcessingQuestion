63ea65fa696473e60398a882c6237789a002ffc4
Nitin Bodkhe <nitinbodkhe07@gmail.com>
2025-08-31 11:17:36 +0530

    Added solution to array processing questions

---
+++ src/Question1.java
@@ -0,0 +1,20 @@
+public class Question1 {
+    public static void main(String[] args) {
+
+        // Question 1 : Total number of elements in an array
+
+        int [] numbers  = {5,7,4,77,4,6,887};
+//        int counter = 0;
+//        for (int i = 0; i < numbers.length; i++) {
+//            counter ++;
+//
+//        }
+
+        int totalElements = numbers.length;
+        System.out.println("Number of elements in the array is: " + totalElements);
+
+
+
+    }
+
+}
